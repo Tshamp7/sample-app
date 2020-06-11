444d2dbeb15631ea4647fb41f4fa35e925e1b40c
Tom Shamp <T.shamp7@gmail.com>
2020-06-11 08:50:15 -0700

    add users page, route, and fill in corresponding link

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 require 'minitest/reporters'
-Minitest::Reporters.use!
+Minitest::Reporters.use!  
 
 
 
@@ -12,6 +12,6 @@ class ActiveSupport::TestCase
 
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
+  include ApplicationHelper
   # Add more helper methods to be used by all tests here...
 end
