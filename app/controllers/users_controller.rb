2a5bb00b7a84b884f84e017f676547a748a3392e
Tom Shamp <T.shamp7@gmail.com>
2020-06-16 09:25:41 -0700

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save #returns true or false
-      flash[:success] = 'Welcome to the Sample App!'
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,4 +15,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     assert_template 'users/new'
   end
+
+  test "valid signup information" do 
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: {name: "example",
+                              email: "rails@example.com",
+                              password: "rubyisgreat",
+                              password_confirmation: "rubyisgreat"} }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+
 end
