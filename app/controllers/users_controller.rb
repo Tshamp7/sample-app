0dc0f7a6d9ba43fc369efa1e77bfabff0f339ba3
Tom Shamp <T.shamp7@gmail.com>
2020-06-16 09:01:48 -0700

    added flash for first time user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,32 @@
 class UsersController < ApplicationController
+
+  # new displays the page for signing up a new user. Found in new.html.erb.
   def new
-    @user = User.new()
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
   end
+
+  # The code below will attempt to save a new user with the data submitted
+  # using the 'new' function. If the save is not successful, then the errors
+  # should be displayed and the 'new' page should be rendered again. 
+  def create
+    @user = User.new(user_params)
+    if @user.save #returns true or false
+      flash[:success] = 'Welcome to the Sample App!'
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password, 
+                                         :password_confirmation)
+    end
 
 end
