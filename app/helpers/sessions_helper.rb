cc21049fe286ba69803ba94a1dac4d402a41034f
Thomas Shamp <T.shamp7@gmail.com>
2020-11-18 21:03:05 -0800

    add sessions controller and login page

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,19 @@
+module SessionsHelper
+
+    # Sets a :user_id key of the session hash to the id of the user passed to the function.
+    def log_in(user)
+        session[:user_id] = user.id
+    end 
+
+    # Sets the current user if none is set. 
+    def current_user 
+        if session[:user_id]
+            @current_user ||= User.find_by(id: session[:user_id])
+        end
+    end
+
+    # Returns true if the current user is logged in. Returns false otherwise.
+    def logged_in?
+        !current_user.nil?
+    end
+end
