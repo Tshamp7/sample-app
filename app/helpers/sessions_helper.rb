14591501ca25b04b62c8421d1c843de12d2cbf99
Thomas Shamp <T.shamp7@gmail.com>
2020-11-19 12:45:43 -0800

    implement advanced login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,12 +3,22 @@ module SessionsHelper
     # Sets a :user_id key of the session hash to the id of the user passed to the function.
     def log_in(user)
         session[:user_id] = user.id
+        # The below line will protect against session replay attacks by resetting the session token to 
+        # the users session token everytime they login. 
+        session[:session_token] = user.session_token
     end 
 
     # Sets the current user if none is set. 
     def current_user 
-        if session[:user_id]
-            @current_user ||= User.find_by(id: session[:user_id])
+        if (user_id = session[:user_id])
+            user = User.find_by(id: user_id) 
+            @current_user ||= user if session[:session_token] == user.session_token
+        elsif (user_id = cookies.encrypted[:user_id])
+            user = User.find_by(id: user_id)
+            if user&.authenticated?(cookies[:remember_token])
+                log_in user
+                @current_user = user
+            end
         end
     end
 
@@ -17,9 +27,27 @@ module SessionsHelper
         !current_user.nil?
     end
 
+    # Forgets persistant session.
+    def forget(user)
+        user.forget
+        cookies.delete(:user_id)
+        cookies.delete(:remember_token)
+    end
+
     # Logs out the current user.
     def log_out
+        forget(current_user)
         reset_session
         @current_user = nil
+    end
+
+    def remember(user)
+        user.remember
+        cookies.permanent.encrypted[:user_id] = user.id
+        cookies.permanent[:remember_token] = user.remember_token
+    end
+
+    def session_token
+        remember_digest || remember
     end
 end
--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -31,6 +31,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     delete logout_path
     assert_not is_logged_in?
     assert_redirected_to root_url
+    delete logout_path #simulate user clicking logout in second browser window.
     follow_redirect!
     assert_select "a[href=?]", login_path
     assert_select "a[href=?]", logout_path, count: 0
@@ -44,5 +45,16 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
     get root_path
     assert flash.empty?
+  end
+
+  test "login with remembering" do  
+    log_in_as(@user, remember_me: '1')
+    assert_equal cookies[:remember_token], assigns(:user).remember_token
+  end
+
+  test 'login without remembering' do
+    log_in_as(@user, remember_me: '1')
+    log_in_as(@user, remember_me: '0')
+    assert_empty cookies[:remember_token]
   end
 end
